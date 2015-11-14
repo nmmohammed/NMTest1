5b86b6d54b6e761f508ebaeade3aa858808b30bf
Nahla Mortada <nahla.hasaneen@yahoo.com>
2015-11-14 15:28:59 +0200

    Inital Commit

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Nahla Mortada on 11/14/15.
+//  Copyright © 2015 Nahla Mortada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
